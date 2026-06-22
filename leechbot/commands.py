ede40f320f8867210169d0911aa81a9be95bd751
Shiniei Nouzen <157171073+Shineii86@users.noreply.github.com>
2026-06-22 11:18:16 +0530

    fix: add missing DUMP_ID import in commands.py

--- leechbot/commands.py
+++ leechbot/commands.py
@@ -21,7 +21,7 @@ from datetime import datetime
 from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from leechbot import app, OWNER, LOG_FILE
+from leechbot import app, OWNER, LOG_FILE, DUMP_ID
 from leechbot.utility.variables import BOT, MSG, YTDL, BotStats, BotTimes, Transfer, Messages, Queue, Paths
 from leechbot.utility.task_manager import task_starter
 from leechbot.utility.helper import (
